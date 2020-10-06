6a9a435f90c7a67745ca075c50f4fbb930976547
Aalyousif <aalyousif@deakin.edu.au>
2020-10-06 16:03:48 +0300

    end of semantic ui usage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Comment, Header } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Comment.Group>
+        <Header as='h3' dividing>
+          SIT313 Comments
+        </Header>
+
+        <Comment>
+          <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+          <Comment.Content>
+            <Comment.Author as='a'>Alyouif</Comment.Author>
+            <Comment.Metadata>
+              <div>Today at 5:42PM</div>
+            </Comment.Metadata>
+            <Comment.Text>How artistic!</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+
+        <Comment>
+          <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+          <Comment.Content>
+            <Comment.Author as='a'>Elliot Fu</Comment.Author>
+            <Comment.Metadata>
+              <div>Yesterday at 12:30AM</div>
+            </Comment.Metadata>
+            <Comment.Text>
+              <p>This has been very useful for my research. Thanks as well!</p>
+            </Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+          <Comment.Group>
+            <Comment>
+              <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+              <Comment.Content>
+                <Comment.Author as='a'>Jenny Hess</Comment.Author>
+                <Comment.Metadata>
+                  <div>Just now</div>
+                </Comment.Metadata>
+                <Comment.Text>Elliot you are always so right </Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          </Comment.Group>
+        </Comment>
+
+
+      </Comment.Group>
   );
 }
 
